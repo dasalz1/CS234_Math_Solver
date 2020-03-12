376523fabd22a5670e7bb885d8e1fae3b9633488
Kamil Ali <kamil.m.ali@outlook.com>
2020-03-11 23:58:40 -0700

    meta learning updated

--- runMeta.py
+++ runMeta.py
@@ -40,7 +40,7 @@ def main(args):
   num_validation_repos = 100
   tb = Tensorboard(args.exp_name, unique_name=args.unique_id)
 
-  data_loaders = [iter(DataLoader(MetaGeneratorDataset(categories=["algebra", "polynomials"]), shuffle=True, batch_size=1))]
+  data_loaders = [iter(DataLoader(MetaGeneratorDataset(categories=["algebra"], num_iterations=1, batch_size=4, k_shot=5), shuffle=True, batch_size=1))]
   validation_data_loaders = [iter(DataLoader(MetaGeneratorDataset(categories=["arithmetic", "probability"]), shuffle=True, batch_size=1))]
 
   # categories = mdsmgr.get_categories()
