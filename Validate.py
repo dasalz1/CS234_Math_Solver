50c14c2ddecb1fe11e6888d2ad049b65d81f26cb
Alekxos <mckeehan@stanford.edu>
2020-03-25 22:51:29 -0700

    First (non-working) implementation of validation script

---
+++ Validate.py
@@ -0,0 +1,75 @@
+from dataset import GeneratorDataset, batch_collate_fn
+from torch.utils import data
+from training import Trainer
+import torch
+import numpy as np
+from tensorboard_utils import tb_mle_batch, tb_policy_batch
+
+category_mappings = {
+    'training': ['algebra', 'arithmetic', 'probability', 'numbers'],
+    'induction': ['polynomials', 'calculus', 'measurement', 'comparison']
+}
+
+@staticmethod
+def validate(model, batch_id, mode = 'training', samples_per_category = 1024, use_mle=True, use_rl = False, tensorboard = None):
+    torch.seed(1111)
+    np.seed(1111)
+    if mode not in category_mappings:
+        mode = 'training'
+        print("Mode not found in category mappings; reverting to default categories.")
+    categories = category_mappings[mode]
+    num_datapoints = samples_per_category * len(categories)
+    validation_datasets = [GeneratorDataset(categories = [categories[category_index]],
+                                          num_iterations=1, batch_size = samples_per_category)
+                                          for category_index in range(len(categories))]
+    validation_loaders = [data.DataLoader(
+        validation_datasets[category_index], batch_size=samples_per_category, shuffle=True,
+        collate_fn=batch_collate_fn)
+        for category_index in range(len(categories))]
+
+    losses_by_category = []
+    n_char_by_category = []
+    n_correct_by_category = []
+
+    if use_mle and not use_rl:
+        for category_index in len(categories):
+            validation_batches = [validation_loaders.[0] for validation_loader in validation_loaders]
+            batch_qs, batch_as = [map(lambda x: x.to(device), validation_batch) for validation_batch in
+                                  validation_batches]
+            loss, n_correct, n_char = Trainer.mle_batch_loss(batch_qs[category_index], batch_as[category_index],
+                                                             model.action_transformer)
+            losses_by_category.append(loss)
+            n_correct_by_category.append(n_correct)
+            n_char_by_category.append(n_char)
+        average_loss = np.mean(losses_by_category)
+        average_n_correct = np.mean(n_correct_by_category)
+        average_n_char = np.mean(n_char_by_category)
+
+        if tensorboard is not None:
+            tb_mle_batch(tensorboard, average_loss, average_n_char, average_n_correct, 0)
+
+
+    elif use_rl and not use_mle:
+        for category_index in len(categories):
+            policy_losses, value_losses, batch_rewards = Trainer.policy_batch_loss(batch_qs, batch_as, model, gamma=0.9)
+            loss, n_correct, n_char = Trainer.mle_batch_loss(batch_qs[category_index], batch_as[category_index],
+                                                             model.action_transformer)
+            losses_by_category.append(loss)
+            n_correct_by_category.append(n_correct)
+            n_char_by_category.append(n_char)
+        average_loss = np.mean(losses_by_category)
+        loss = policy_losses + value_losses
+        reward = np.meanb
+        if tensorboard is not None:
+            tb_policy_batch(tensorboard, batch_rewards, value_losses, 0, 0, num_datapoints)
+
+    return
+
+    '''
+    
+    '''
+
+
+def meta_validate():
+    pass
+
