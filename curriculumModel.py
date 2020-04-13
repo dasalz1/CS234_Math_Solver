943ce4021af5e0f687b73361aad3dde038e68eee
Alekxos <mckeehan@stanford.edu>
2020-04-13 09:40:41 -0700

    Restore changes to curriculumModel

--- curriculumModel.py
+++ curriculumModel.py
@@ -6,8 +6,19 @@ class CurriculumNetwork(nn.Module):
     def __init__(self, input_size, output_size, hidden_layer_size=256):
 
         super(CurriculumNetwork, self).__init__()
-        self.layer1 = nn.Linear(input_size, hidden_layer_size)
-        self.layer2 = nn.Linear(hidden_layer_size, output_size)
+        hidden_layer_size = 128
+        self.input_size = input_size
+        self.output_size = output_size
+        assert(input_size == output_size)
+        # For curriculum network, input and output size should be same (category performance->category percentage)
+        self.layer1 = nn.Linear(self.input_size, hidden_layer_size)
+        self.layer2 = nn.Linear(hidden_layer_size, self.output_size)
 
-    def forward(self, teacher_input):
-        return F.softmax(self.layer2(F.relu(self.layer1(teacher_input))), dim=-1)
+    def forward(self, input):
+        model = torch.nn.Sequential(
+            self.layer1,
+            nn.ReLU(),
+            self.layer2,
+            nn.Softmax(dim=-1)
+        )
+        return model(input)
